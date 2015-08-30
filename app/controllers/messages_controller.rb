8bbf80c851516fd0977959e2084251a5e5d1e6d4
yasuhisa1984 <penmouse275@icloud.com>
2015-08-30 12:48:51 +0000

    second commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -44,7 +44,7 @@ class MessagesController < ApplicationController
   private
   def message_params
     
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   
   
